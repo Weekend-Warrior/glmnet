09d3aea588cd3c3fa8dddfb5b56091e3d825d922
Stewge <stewbobb@gmail.com>
2018-04-28 10:30:57 -0500

    Update Cox PH type.measures with AUC on the censor variable "status".

--- R/cv.coxnet.R
+++ R/cv.coxnet.R
@@ -2,11 +2,12 @@ cv.coxnet <-
 function (outlist, lambda, x, y, weights, offset, foldid, type.measure, 
     grouped, keep = FALSE) 
 {
-    typenames = c(deviance = "Partial Likelihood Deviance")
+    typenames = c(deviance = "Partial Likelihood Deviance",
+                  auc = 'AUC')
     if (type.measure == "default") 
         type.measure = "deviance"
-    if (!match(type.measure, c("deviance"), FALSE)) {
-        warning("Only 'deviance'  available for Cox models; changed to type.measure='deviance'")
+    if (!match(type.measure, c("deviance", 'auc'), FALSE)) {
+        warning("Only 'deviance' and 'auc' available for Cox models; changed to type.measure='deviance'")
         type.measure = "deviance"
     }
     if (!is.null(offset)) {
@@ -14,7 +15,15 @@ function (outlist, lambda, x, y, weights, offset, foldid, type.measure,
         offset = drop(offset)
     }
     else is.offset = FALSE
+    N = nrow(y)
     nfolds = max(foldid)
+    
+    if ((N/nfolds < 10) && type.measure == "auc") {
+      warning("Too few (< 10) observations per fold for type.measure='auc' in cv.coxnet; changed to type.measure='deviance'. Alternatively, use smaller value for nfolds",
+              call. = FALSE)
+      type.measure = "deviance"
+    }
+    
     if ((length(weights)/nfolds < 10) && !grouped) {
         warning("Option grouped=TRUE enforced for cv.coxnet, since < 3 observations per fold", 
             call. = FALSE)
@@ -24,13 +33,27 @@ function (outlist, lambda, x, y, weights, offset, foldid, type.measure,
 ###We dont want to extrapolate lambdas on the small side
   mlami=max(sapply(outlist,function(obj)min(obj$lambda)))
   which_lam=lambda >= mlami
-  if (keep) predmat = matrix(NA, nrow(y), length(lambda))
- 
+  if (keep | type.measure == 'auc') predmat = matrix(NA, nrow(y), length(lambda))
+  if (type.measure == 'auc') {
+    good = matrix(0, nfolds, length(lambda))
+  }
     for (i in seq(nfolds)) {
         which = foldid == i
         fitobj = outlist[[i]]
-        coefmat = predict(fitobj, type = "coeff",s=lambda[which_lam])
-        if (grouped) {
+        
+        if (keep | type.measure == 'auc') {
+          nlami = sum(which_lam)
+          predmat[which, seq(nlami)] = predict(fitobj, x[which, , drop = FALSE], s=lambda[which_lam], newoffset = offset[which])
+        }
+        
+        if(type.measure == 'auc'){
+          good[i, seq(nlami)] = 1
+          for (j in seq(nlami)) {
+            cvraw[i, j] = auc(y[which, 'status'], -predmat[which, j], weights[which])
+          }
+        }
+        else if (grouped) {
+            coefmat = predict(fitobj, type = "coeff",s=lambda[which_lam])
             plfull = coxnet.deviance(x = x, y = y, offset = offset, 
                 weights = weights, beta = coefmat)
             plminusk = coxnet.deviance(x = x[!which, ], y = y[!which, 
@@ -39,21 +62,23 @@ function (outlist, lambda, x, y, weights, offset, foldid, type.measure,
             cvraw[i, seq(along = plfull)] = plfull - plminusk
         }
         else {
+            coefmat = predict(fitobj, type = "coeff",s=lambda[which_lam])
             plk = coxnet.deviance(x = x[which, ], y = y[which, 
                 ], offset = offset[which], weights = weights[which], 
                 beta = coefmat)
             cvraw[i, seq(along = plk)] = plk
         }
-        if (keep) {
-          preds = predict(fitobj, x[which, , drop = FALSE], s=lambda[which_lam], newoffset = offset[which])
-          nlami = sum(which_lam)
-          predmat[which, seq(nlami)] = preds
-        }
     }
     status = y[, "status"]
-    N = nfolds - apply(is.na(cvraw), 2, sum)
-    weights = as.vector(tapply(weights * status, foldid, sum))
-    cvraw = cvraw/weights
+    if (type.measure == 'auc') {
+      N = apply(good, 2, sum)
+      weights = as.vector(tapply(weights, foldid, sum))
+    }
+    else {
+      N = nfolds - apply(is.na(cvraw), 2, sum)
+      weights = as.vector(tapply(weights * status, foldid, sum))
+      cvraw = cvraw/weights
+    }
     cvm = apply(cvraw, 2, weighted.mean, w = weights, na.rm = TRUE)
     cvsd = sqrt(apply(scale(cvraw, cvm, FALSE)^2, 2, weighted.mean, 
         w = weights, na.rm = TRUE)/(N - 1))
